cb864e14cd270001871c3cccfb1220b8941995ed
Sajjad <rizvisajjad80@gmail.com>
2026-07-02 22:45:18 +0530

    Halfway through contracts module

---
+++ backend/src/modules/contracts/contract.schema.js
@@ -0,0 +1,18 @@
+import {z} from 'zod'
+
+export const createContractSchema = z.object({
+    title: z.string().min(3),
+    description: z.string().min(10),
+    totalAmount: z.number().positive(),
+    freelancerId: z.string().uuid(),
+    milestones: z.array(z.object({
+        title: z.string().min(),
+        description: z.string().min(10),
+        amount: z.number().positive(),
+        dueDate: z.string().datetime()
+    })).min(1)
+})
+
+export const updateContractStatusSchema = z.object({
+    status: z.enum(['ACTIVE', 'CANCELLED'])
+})
