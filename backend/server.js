7ece3a2cfe426b94257ca216a570ccb18d155959
Sajjad <rizvisajjad80@gmail.com>
2026-07-13 12:56:13 +0530

    Mounted the ghost job when starting server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import app from './app.js'
 import config from './src/config/index.js'
+import { scheduleGhostCheck } from './src/jobs/ghost.worker.js'
 
 
 
-app.listen(config.PORT, ()=>{
+app.listen(config.PORT, async ()=>{
     console.log(`Server is running at ${config.PORT}`)
+    await scheduleGhostCheck()
+    console.log('Ghost detection Schedule')
 })
